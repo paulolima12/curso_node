65e3efbd84e223329f85f4664c3565a3ac8af96c
paulolima12 <hrpaulo1204@gmail.com>
2024-06-06 09:02:45 -0300

    arruma4r tudo depois kksksksk

--- 04_html/teste.js
+++ 04_html/teste.js
@@ -4,10 +4,39 @@ const sequelize = new Sequelize('test','root','',{
     dialect: 'mysql'
 })
 
-sequelize.authenticate().then(function(){
-    console.log('conectado com sucesso')
+// sequelize.authenticate().then(function(){
+//     console.log('conectado com sucesso')
 
-}).cath(function(erro){
-    console.log('erro pae:' + erro)
+// }).cath(function(erro){
+//     console.log('erro pae:' + erro)
 
+// })
+
+const Postagem = sequelize.define('Postagens'),{
+    titulo:{
+        type: Sequelize.STRING
+    },
+
+    contudo :{
+
+
+    }
+
+}
+.postagem.sync(force = true)
+Usario
+
+
+
+
+
+
+
+
+
+
+
+Postagem.create({
+    titulo:'primeira postagem',
+    conteudo :'Conteudo da primeira postagem, babababab'
 })
